1a7c077b60a5074ecf5e6f88b1bc42454444b207
hiba <>
2024-10-28 23:28:50 +0100

    connect postgres

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+"""
+calculator function for
+
+
+
+
+"""
+    
+def add(x,y):
+    """add x and y """
+    return x+y
+
+
+def sub(x,y):
+    """Subtract number"""
+    return y-x
---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+from app import calc
+    
+class CalcTest(SimpleTestCase):
+    def test_add_numbers(self):
+        """Test adding two numbers together"""
+        res = calc.add(5,6)
+        self.assertEqual(res , 11)
+        
+    def test_sub_numbers(self):
+        """Test the sub""" 
+        res = calc.sub(10,15)
+        self.assertEqual(res,5)   
